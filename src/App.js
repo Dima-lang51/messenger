d8dfd029f6993b984a667bd74e4e0c046d305acc
Dima-lang51 <68560471+Dima-lang51@users.noreply.github.com>
2022-04-03 21:16:18 +0300

    Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Message } from './components/Message';
+import { Form } from './components/Form';
 import './styles/App.css';
 
+
+const name = 'me';
+const msgs = [];
+
 function App() {
-  const foo = () => {
-    alert('Привет');
-  }
+
+  const [messages, setMessages] = useState(msgs);
+
+  const addMessage = (newText) => {
+    setMessages([...messages, {text: newText, author: name}]);
+  };
+
+  useEffect(() => {
+    if (messages.length && messages[messages.length-1].author === name) {
+      setMessages([...messages, {text: 'hi', author: 'robot'}]);
+    }
+  }, [messages]);
+ 
   return (
     <div className="App">
+
       <header className='header'>
         <div className='container'>
-          <Message name="№" number={1} dosmth={foo}/>
+          {messages.map((msg) => (
+            <Message text={msg.text} author={msg.author} />
+          ))}
         </div>
       </header>
-      <main className='main'></main>
+
+      <main className='main'>
+        <div className='container'>
+          <Form onSubmit={addMessage}/>
+        </div>
+      </main>
+
       <footer className='footer'></footer>
+
     </div>
   );
 }
